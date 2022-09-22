dda8c7ad019361f62a0fbaebdc06b6dd4335e6f2
Emanuel Victor Fontoura Cantalejo <103543476+EmanuelVFC@users.noreply.github.com>
2022-09-22 14:22:30 -0300

    continuacao

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var article_aviao = window.document.querySelector("article.viagem-aviao")
+var article_hotel = window.document.querySelector("article.viagem-hotel")
+var article_rental = window.document.querySelector("article.viagem-rental")
+
